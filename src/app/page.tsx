e9c4209c4f5b41bd657c6a0fca774769cc2a112a
yann <yann.thibault1@gmail.com>
2023-10-20 11:22:44 +0200

    fix fetch address

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import {Box, VStack, Text, Spacer, Input} from '@chakra-ui/react';
 import CollectionList from '../components/CollectionList';
 
 async function loadCollections() {
-    const res = await fetch('http://localhost:3000/api/collections');
+    const res = await fetch('http://127.0.0.1:3000/api/collections');
     const data = await res.json();
     const collectionsData = data.collections.map((c: any) => ({
         address: c.address,
